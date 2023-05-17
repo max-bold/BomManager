96deecfa30faa0453e822fbac7fad5b9eec98bca
boldm <bm456@ya.ru>
2023-05-17 21:10:23 +0400

    Removed some commented parts

--- Fusion/Scripts/BomToJASON/BomToJASON.py
+++ Fusion/Scripts/BomToJASON/BomToJASON.py
@@ -41,27 +41,17 @@ def getbom(root: adsk.fusion.Component):
 
 def run(context):
     ui = None
-    # try:
-    app = adsk.core.Application.get()
-    ui = app.userInterface
-    # ui.messageBox('hello')
-    # print('hello')
-
-    product = app.activeProduct
-    design = adsk.fusion.Design.cast(product)
-    root = design.rootComponent
-    # Print path to current working directory
-    # print(os.getcwd())
-    bom = getbom(root)
-    # print(bom)
-    with open('E:/CODE/BomManager/Fusion/Scripts/BomToJASON/bom.json','w', encoding='utf16') as out:
-        #Write dictionary to json python
-        _as_json = json.dumps(bom, indent=4, ensure_ascii=False)
-        out.write(_as_json)
-    print('Done')
-
-
-
-    # except:
-    #     if ui:
-    #         ui.messageBox("Failed:\n{}".format(traceback.format_exc()))
+    try:
+        app = adsk.core.Application.get()
+        ui = app.userInterface
+        product = app.activeProduct
+        design = adsk.fusion.Design.cast(product)
+        root = design.rootComponent
+        bom = getbom(root)
+        with open('E:/CODE/BomManager/Fusion/Scripts/BomToJASON/bom.json','w', encoding='utf16') as out:
+            _as_json = json.dumps(bom, indent=4, ensure_ascii=False)
+            out.write(_as_json)
+        print('Done')
+    except:
+        if ui:
+            ui.messageBox("Failed:\n{}".format(traceback.format_exc()))
